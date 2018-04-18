221b80037982363060037fe2b75329fc4799a6cb
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-18 05:16:34 -0700

    set up db connection

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 const app = require('./lib/app');
+const connect = require('./lib/connect');
 
 const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/yarns';
+
+connect(MONGODB_URI);
 
 const server = http.createServer(app);
 
